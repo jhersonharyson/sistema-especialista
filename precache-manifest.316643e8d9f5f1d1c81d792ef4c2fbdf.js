b671f959330cfb9391dd2cb6ed60e61a98347977
Jherson Haryson <37131507+jhersonharyson@users.noreply.github.com>
2018-11-22 10:43:45 -0200

    upload da v1 do gerador de sistema especialista
    a versão ainda não foi finalizada

---
+++ precache-manifest.316643e8d9f5f1d1c81d792ef4c2fbdf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8667ecd9c51d6185b69f",
+    "url": "/static/js/main.8667ecd9.chunk.js"
+  },
+  {
+    "revision": "d12d9b41c95200e619ad",
+    "url": "/static/js/1.d12d9b41.chunk.js"
+  },
+  {
+    "revision": "8667ecd9c51d6185b69f",
+    "url": "/static/css/main.baaadcbf.chunk.css"
+  },
+  {
+    "revision": "66f3bc80c6d39ef5abd1a96776341253",
+    "url": "/index.html"
+  }
+];
