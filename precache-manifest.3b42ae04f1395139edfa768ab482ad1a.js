04cfeb48f87eeeab535862ec0ae9e5108f74fc03
Jherson Haryson <37131507+jhersonharyson@users.noreply.github.com>
2018-12-02 00:23:45 -0200

    remoção do /

--- precache-manifest.3b42ae04f1395139edfa768ab482ad1a.js
+++ precache-manifest.3b42ae04f1395139edfa768ab482ad1a.js
@@ -1,7 +1,7 @@
 self.__precacheManifest = [
   {
     "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
-    "url": "/static/media/logo.5d5d9eef.svg"
+    "url": "static/media/logo.5d5d9eef.svg"
   },
   {
     "revision": "229c360febb4351a89df",
